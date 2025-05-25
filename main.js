9bcf6a5250a999239888757c5b5ba050a0b95616
Kerem. <111786677+kereMerek@users.noreply.github.com>
2025-05-26 00:56:07 +0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 onload = () => {
-    const c = setTimeout(() => {
-      document.body.classList.remove("not-loaded");
-      clearTimeout(c);
-    }, 1000);
-  };
+  const c = setTimeout(() => {
+    document.body.classList.remove("not-loaded");
+
+    if (window.innerWidth <= 768) {
+      // Mobil cihazlar için
+      document.body.classList.add("mobile");
+    } else {
+      // Masaüstü için
+      document.body.classList.remove("mobile");
+    }
+
+    clearTimeout(c);
+  }, 1000);
+};
